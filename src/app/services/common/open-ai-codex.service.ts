e11b443bde8634f7f40b2c5b66df057274186b04
cem can <cmcan@windowslive.com>
2024-09-11 17:36:20 +0300

    Main | Accident adn Monthly Directorate Components interface are fixed. Chart null data showwn issues fixed.

--- src/app/services/common/open-ai-codex.service.ts
+++ src/app/services/common/open-ai-codex.service.ts
@@ -1,11 +1,1 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class OpenAiCodexService {
-
-  }
 
