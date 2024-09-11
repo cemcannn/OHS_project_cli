fd3732b4099d369be0450ed1a93402129a73eaf8
cem can <cmcan@windowslive.com>
2024-09-11 17:35:47 +0300

    Main | Accident adn Monthly Directorate Components interface are fixed. Chart null data showwn issues fixed.

--- src/app/services/common/open-ai-codex.service.ts
+++ src/app/services/common/open-ai-codex.service.ts
@@ -6,30 +6,6 @@ import { catchError, Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class OpenAiCodexService {
-  private apiUrl = 'https://api.openai.com/v1/completions';
 
-
-  constructor(private httpClient: HttpClient) { }
-
-  getCodexCompletion(prompt: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${this.apiKey}`  // Bearer token olarak API anahtarını ekle
-    });
-
-    const body = {
-      model: 'davinci-002',  // OpenAI Codex modeli
-      prompt: prompt,
-      max_tokens: 150,
-      temperature: 0
-    };
-
-    return this.httpClient.post<any>(this.apiUrl, body, { headers }).pipe(
-        catchError(error => {
-          console.error('Error occurred:', error);  // Konsolda hata mesajını yazdır
-          return throwError(() => new Error('An error occurred while fetching the completion'));  // Hata mesajını döndür
-        })
-      );
-    }
   }
 
