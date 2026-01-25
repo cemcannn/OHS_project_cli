771a07fcd0d8d06fc2de10a0a5831121f4d11d90
cemcannn <cmcan@windowslive.com>
2026-01-25 20:47:51 +0300

    Fix environment import path
    Use Angular standard src/environments/environment import.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { enableProdMode, LOCALE_ID } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
 // Türkçe yerelleştirme
 import '@angular/common/locales/global/tr';
-import { environment } from './enviroments/enviroment.prod';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
